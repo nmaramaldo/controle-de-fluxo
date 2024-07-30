94279a8685963a21b470e4bf1978994b13c45ec5
Natália <nataliamaramaldof@gmail.com>
2024-07-29 21:07:53 -0300

    Delete ControleDeFluxo/controle-de-fluxo/ControleDeFluxo directory

--- ControleDeFluxo/controle-de-fluxo/ControleDeFluxo/src/ParametrosInvalidosException.java
+++
@@ -1,6 +0,0 @@
-// ParametrosInvalidosException.java
-public class ParametrosInvalidosException extends Exception {
-    public ParametrosInvalidosException(String mensagem) {
-        super(mensagem);
-    }
-}
