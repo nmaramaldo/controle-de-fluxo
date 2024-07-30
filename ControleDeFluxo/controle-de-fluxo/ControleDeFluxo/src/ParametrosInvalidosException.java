8cb2dc13ac6b7d9f4b9d3dab70c480ab09d368c5
Natália <nataliamaramaldof@gmail.com>
2024-07-29 21:06:40 -0300

    Add files via upload

---
+++ ControleDeFluxo/controle-de-fluxo/ControleDeFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+// ParametrosInvalidosException.java
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
